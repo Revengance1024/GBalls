e139e6378fc6080b306f0608c611a265d844cfb0
Revengance1024 <andrisb.jun@inbox.lv>
2015-07-28 15:59:27 +0300

    Initial commit

--- core/src/com/litleman/gballs/GBalls.java
+++ core/src/com/litleman/gballs/GBalls.java
@@ -2,22 +2,38 @@ package com.litleman.gballs;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import com.litleman.gballs.input.Controls;
+
 public class GBalls extends ApplicationAdapter {
+	OrthographicCamera camera;
 	SpriteBatch batch;
+	InputProcessor inputProcessor;
+	BitmapFont defaultFont;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		defaultFont = new BitmapFont();
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
+		inputProcessor = new Controls();
+		Gdx.input.setInputProcessor(inputProcessor);
 
 	}
 
 	@Override
 	public void dispose(){
-
+		batch.dispose();
+		defaultFont.dispose();
 	}
 
 	@Override
@@ -29,6 +45,10 @@ public class GBalls extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+			defaultFont.draw(batch, Gdx.input.getX() + " : " + Gdx.input.getY(), 100, 100);
+		batch.end();
 	}
 
 	@Override
