48d4270ccc38b184fc4a19332e7b1149127482a2
Revengance1024 <andrisb.jun@inbox.lv>
2015-07-28 14:30:55 +0300

    Initial commit

---
+++ android/src/com/litleman/gballs/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.litleman.gballs.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.litleman.gballs.GBalls;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GBalls(), config);
+	}
+}
---
+++ desktop/src/com/litleman/gballs/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.litleman.gballs.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.litleman.gballs.GBalls;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GBalls(), config);
+	}
+}
